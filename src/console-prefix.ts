f62297bc7a0f9699c4ede811f59e1f2809c85497
ahmadaidil <ahmdaidil@gmail.com>
2019-09-12 19:13:51 +0700

    support any / template prefix param type

--- src/console-prefix.ts
+++ src/console-prefix.ts
@@ -1,4 +1,7 @@
-export = (prefix: string): void => {
+/**
+ * @param {T} prefix
+ */
+export = <T>(prefix: T): void => {
   const consoleTypes = {
     log: console.log,
     error: console.error,
@@ -7,10 +10,15 @@ export = (prefix: string): void => {
     debug: console.debug
   }
 
+  function setPrefix(type: string) {
+    const prefixMsg = typeof prefix === 'function' ? prefix() : prefix
+    return `[${type.toUpperCase()}][${prefixMsg}]`
+  }
+
   Object.keys(consoleTypes).forEach(key => {
     const type = key as keyof typeof consoleTypes
     console[type] = function(...args: any[]) {
-      args.unshift(`[${type.toUpperCase()}][${prefix}]`)
+      args.unshift(setPrefix(type))
       consoleTypes[type](...args)
     }
   })
